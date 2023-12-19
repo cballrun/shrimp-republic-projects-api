563bd0175480f23a74ddf03729900b4baab13fd2
cballrun <carter.k.ball@gmail.com>
2023-12-18 19:58:09 -0500

    Complete model tests

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,5 @@
 class Tool < ApplicationRecord
-    
-
+    has_many :project_tools
+    has_many :projects, :through => :project_tools
+    validates_presence_of :name, :url
 end
--- spec/models/project_tool_spec.rb
+++ spec/models/project_tool_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ProjectTool, type: :model do
   describe 'relationships' do
-
+    it { should belong_to :project }
+    it { should belong_to :tool }
   end 
-
-  describe 'validations' do
-  end
 end
